f88212c94045ef285e54719da7ab29cc335f98a3
Карина Алексеевна Золотова <karinovirys19@gmail.com>
2024-09-19 09:37:34 +0500

    Task2

---
+++ Tyuiu.ZolotovaKA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ZolotovaKA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.ZolotovaKA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Карина";
+            var ress = DataService.GetMessage(name);
+            Assert.AreEqual("Привет..., Карина", ress);
+        }
+    }
+}
